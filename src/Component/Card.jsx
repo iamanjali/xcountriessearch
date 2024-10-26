fb8356ed1f2bc858af8bf2189a157567100483d0
iamanjali <iamanjalijaiswal@gmail.com>
2024-10-26 15:47:38 +0530

    Updated Search countries

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -1,12 +1,12 @@
-import styles from "./Card.module.css"
+import styles from "./Card.module.css";
 
-function Card ({name, flag}) {
-    return (
-        <div className={styles.countryCard}>
-        <img src={flag} alt={`Flag of ${name}`} width="100" height="100"/>
-        <h2>{name}</h2>
+function Card({ name, flag }) {
+  return (
+    <div className={styles.countryCard}>
+      <img src={flag} alt={name} width="100" height="100" />
+      <h2>{name}</h2>
     </div>
-    )
+  );
 }
 
 export default Card;
